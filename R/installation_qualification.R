004d9660b72a3e4c515270c44e9e67230d1011a0
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2026-08-26 09:56:16 +0200

    #26 Review method run_qualification.

--- R/installation_qualification.R
+++ R/installation_qualification.R
@@ -242,21 +242,12 @@ run_qualification <- function(
 ) {
   if (
     !all(
-      packages %in% c("campsismod", "campsis", "campsisnca", "campsismisc", "campsisqual", "campsistrans", "ecampsis")
+      packages %in% c("campsismod", "campsis", "campsisnca", "campsismisc", "campsisqual", "campsistrans")
     )
   ) {
     stop("Invalid packages. Only packages from the Campsis suite can be qualified.")
   }
 
-  if (length(find.package("ncappc", quiet = TRUE)) == 0) {
-    stop("ncappc not installed")
-  }
-  if (length(find.package("tinytex", quiet = TRUE)) == 0) {
-    stop("tinytex not installed")
-  }
-  if (length(find.package("tictoc", quiet = TRUE)) == 0) {
-    stop("tictoc not installed")
-  }
   if (length(find.package("mrgsolve", quiet = TRUE)) == 0) {
     stop("mrgsolve not installed")
   }
